246e3fdf34eb00cef0ba62e7bc3771e3ea7cd0b6
siying <siyinglyu@gmail.com>
2019-01-25 01:13:28 -0600

    template finished

--- src/products/models.py
+++ src/products/models.py
@@ -3,6 +3,7 @@ import random
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 from .utils import unique_slug_generator
+from django.urls import reverse
 
 def get_filename_ext(filepath):
 	base_name = os.path.basename(filepath)
@@ -48,11 +49,13 @@ class Product(models.Model):
 	image		= models.ImageField(upload_to=upload_image_path, null=True, blank=True)
 	featured	= models.BooleanField(default=False)
 	active		= models.BooleanField(default=True)
+	timestamp	= models.DateTimeField(auto_now_add=True)
 
 	objects		= ProductManager() # extend the default method
 
 	def get_absolute_url(self):
-		return "/products/{slug}/".format(slug=self.slug)
+		# return "/products/{slug}/".format(slug=self.slug)
+		return  reverse("products:detail", kwargs={"slug": self.slug})
 		
 	# change the product object with the title string
 	def __str__(self):
