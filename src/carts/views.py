b815effea4bed18e17965e3e190f03a354083f0a
siying <siyinglyu@gmail.com>
2019-02-01 00:19:46 -0600

    cart finished

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,11 +1,51 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from accounts.models import GuestEmail
+from accounts.forms import LoginForm, GuestForm
+from .models import Cart
+from products.models import Product
+from orders.models import Order
+from billing.models import BillingProfile
 
 def cart_home(request):
-	cart_id = request.session.get("cart_id", None)
-	if cart_id is None: # and ininstance(cart_id, int):
-		print('create a new cart')
-		request.session['cart_id'] = 12
-	else:
-		print('Cart ID exists, this is existing')
-	return render(request, "carts/home.html", {})
+	cart_obj, new_obj = Cart.objects.new_or_get(request)
+
+	return render(request, "carts/home.html", {"cart": cart_obj})
+
+def cart_update(request):
+	print(request.POST)
+	product_id = request.POST.get('product')
+	if product_id is not None:
+		try:
+			product_obj = Product.objects.get(id=product_id)
+		except Product.DoseNotExist:
+			print("Show message to user no product")
+			return redirect("cart:home")
+		cart_obj, new_obj = Cart.objects.new_or_get(request)
+		if product_obj in cart_obj.products.all():
+			cart_obj.products.remove(product_obj)
+		else:
+			cart_obj.products.add(product_obj)
+		request.session['cart_items'] = cart_obj.products.count()
+	# return redirect(product_obj.get_absolute_url())
+	return redirect("cart:home")
+
+def checkout_home(request):
+	cart_obj, cart_created = Cart.objects.new_or_get(request)
+	order_obj = None
+	if cart_created or cart_obj.products.count() == 0:
+		return redirect("cart:home")
+	
+	login_form = LoginForm()
+	guest_form = GuestForm()
+
+	billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+	if billing_profile is not None:
+		order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
+
+	context = {
+		'object': order_obj,
+		'billing_profile': billing_profile,
+		'login_form': login_form,
+		'guest_form': guest_form
+	}
+	return render(request, "carts/checkout.html", context)
